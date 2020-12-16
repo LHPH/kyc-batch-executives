4aeb598a9aebc7be844625674bb4136967f788f0
Luis H <luislyly@hotmail.com>
2020-12-15 22:52:32 -0600

    Add empty project

---
+++ src/main/java/com/kyc/batchs/KycBatchsApplication.java
@@ -0,0 +1,13 @@
+package com.kyc.batchs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KycBatchsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KycBatchsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kyc/batchs/KycBatchsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kyc.batchs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KycBatchsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
